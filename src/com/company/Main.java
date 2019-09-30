f7f5e541910b69c3d57ce83d2d60d4d40880d8e7
bitziu <kurame8@gmail.com>
2019-09-30 23:10:11 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	final String A = "Hello";
+	String b ="World";
+	String c =A+b;
+	System.out.println(c);
+    }
+}
